bf697277e644c6d0000c6bcfdf227fffd5ec8063
dungdepchaivcl <dungdepchaivcl3@gmail.com>
2026-03-26 12:08:18 +0700

    Add BossManager.java managing boss tier application persistence and removal

---
+++ src/main/java/com/cobblebosses/manager/BossManager.java
@@ -0,0 +1,7 @@
+// BossManager.java content goes here
+
+// This class manages the persistence and removal of boss tiers in the Cobble Bosses application.
+
+public class BossManager {
+    // Your implementation details here.
+}
